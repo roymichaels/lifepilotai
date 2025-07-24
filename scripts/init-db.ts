139c3ed3eb8a62fe3460e5b2722ec2823ae5b9d4
roymichaels <RoyMichaelsDotCom@pm.me>
2025-07-25 04:13:46 +0700

    Fix DB init script

--- scripts/init-db.ts
+++ scripts/init-db.ts
@@ -1,16 +1,14 @@
-import path from 'path'
+import { resolve } from 'node:path'
 
 // Database initialization using ElectricSQL migrations
-
 async function run() {
   try {
-    const { Database } = await import('wa-sqlite')
-    const { initSQLite } = await import('../src/lib/sqlite')
+    const { ElectricDatabase, electrify } = await import('electric-sql/wa-sqlite')
+    const { schema } = await import('../src/sqlite/migrations')
 
-    const dbPath = path.resolve(__dirname, '../blue-ocean.db')
-    const db = new Database(dbPath)
-    await initSQLite(db)
-    await db.close()
+    const dbName = resolve(__dirname, '../blue-ocean.db')
+    const db = await ElectricDatabase.init(dbName)
+    await electrify(db, schema)
     console.log('Database initialized')
   } catch (err) {
     console.error('Failed to initialise database', err)
--- src/sqlite/migrations.ts
+++ src/sqlite/migrations.ts
@@ -1,4 +1,4 @@
-export const schema = {
+export const schema: any = {
   version: 1,
   tables: {},
 }
